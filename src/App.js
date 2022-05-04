8f36d46e9f82101c266a2df7c7ee37a513a9953e
ShadePrima <aleksandrag1984@gmail.com>
2022-05-04 17:37:44 +0300

    add thry clock

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import './App.css';
+import React from 'react';
 // import Clock from './components/Clock';
 // import Comment from './components/Comment';
 
 function App() {
-  // return (
+  return (
+    <div>
+    <Clock />
+    <Clock />
+    <Clock />
+    </div>
+
+
+
   //   // <p>Hello word</p>,
-  //   // <Clock />
   //   // <Comment
   //   //   date={comment.date}
   //   //   text={comment.text}
   //   //   author={comment.author}
   //   // />
-  //   );
+    );
 
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state = {date: new Date()}
+  }
+
+  componentDidMount() {
+      this.timerID = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+      clearInterval(this.timerID)
+  }
+
+  tick() {
+      this.setState({
+          date: new Date()
+      })
+  }
+
+  render () {
+      return (
+          <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+          </div>
+        );
+  }
 }
 
 // const comment = {
--- src/index.js
+++ src/index.js
@@ -1,43 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render (<Clock />)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+
+
+// class Clock extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {date: new Date()}
+//     }
+
+//     componentDidMount() {
+//         this.timerID = setInterval(() => this.tick(), 1000)
+//     }
+
+//     componentWillUnmount() {
+//         clearInterval(this.timerID)
+//     }
+
+//     tick() {
+//         this.setState({
+//             date: new Date()
+//         })
+//     }
+
+//     render () {
+//         return (
+//             <div>
+//               <h1>Hello, world!</h1>
+//               <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//             </div>
+//           );
+//     }
+// }
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render (<Clock />)
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// );
-
-
-
-class Clock extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {date: new Date()}
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(() => this.tick(), 1000)
-    }
-
-    componentWillMount() {
-        clearInterval(this.timerID)
-    }
-
-    render () {
-        return (
-            <div>
-              <h1>Hello, world!</h1>
-              <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-          );
-    }
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render (<Clock />)
   
