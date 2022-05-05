ac0790c9813f86d6a40081e2590de8b1e9587bc5
ShadePrima <aleksandrag1984@gmail.com>
2022-05-05 12:47:21 +0300

    add new component clock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-// import Clock from './components/Clock';
+import Clock from './components/Clock';
 // import Comment from './components/Comment';
 
 function App() {
@@ -9,56 +9,10 @@ function App() {
     <Clock />
     <Clock />
     <Clock />
+    <p>Hello word</p>
     </div>
-
-  //   // <p>Hello word</p>,
-  //   // <Comment
-  //   //   date={comment.date}
-  //   //   text={comment.text}
-  //   //   author={comment.author}
-  //   // />
-    );
+  );
 
 }
-
-class Clock extends React.Component {
-  constructor(props) {
-      super(props)
-      this.state = {date: new Date()}
-  }
-
-  componentDidMount() {
-      this.timerID = setInterval(() => this.tick(), 1000)
-  }
-
-  componentWillUnmount() {
-      clearInterval(this.timerID)
-  }
-
-  tick() {
-      this.setState({
-          date: new Date()
-      })
-  }
-
-  render () {
-      return (
-          <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-          </div>
-        );
-  }
-}
-
-// const comment = {
-//   date: new Date(),
-//   text: 'I hope you enjoy learning React!',
-//   author: {
-//     name: 'Hello Kitty',
-//     avatarUrl: 'http://placekitten.com/g/64/64'
-//   }
-// };
-
 
 export default App;
